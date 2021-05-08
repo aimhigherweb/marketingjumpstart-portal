757f0e5ec27a415dc0d4e4d35a3cd369c7760cd6
Amy Kapernick <amy@aimhigherweb.design>
2021-05-08 19:16:37 +0800

    removed fetch on dashboard

--- components/partials/dashboard/index.js
+++ components/partials/dashboard/index.js
@@ -1,38 +1,11 @@
 import { Fragment, useContext } from 'react';
 
-import DocsList from '../../parts/docsList';
-import ClientDashboard from '../../dashboard/client';
-
-import GraphQLFetch, { CMSDataContext } from '../../parts/fetchData';
-
-import { FILTER_DOCS } from '../../../utils/cms/docs/index';
-
 import styles from './dashboard.module.scss';
 
-const Dashboard = () => {
-	const { clients } = useContext(CMSDataContext);
-	const query = {
-		QUERY: FILTER_DOCS,
-		options: {
-			variables: {
-				clients: clients.map((client) => client.slug)
-			}
-		},
-	};
-
-	return (
-		<div className={styles.dashboard} style={{ '--grid_rows': clients.length }}>
-			{clients.map((client) => (
-				<ClientDashboard key={client.slug} {...client} />
-			))}
-			<GraphQLFetch {...query}>
-				<div className={styles.docs}>
-					<h2>Docs</h2>
-					<DocsList />
-				</div>
-			</GraphQLFetch>
-		</div>
-	);
-};
+const Dashboard = () => (
+	<div className={styles.dashboard}>
+		<h1>Dashboard</h1>
+	</div>
+);
 
 export default Dashboard;
