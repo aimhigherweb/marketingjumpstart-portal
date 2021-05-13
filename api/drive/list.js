a2113feb056e80421172fee68141d5c3832d4602
Amy Kapernick <amy@aimhigherweb.design>
2021-05-13 16:50:28 +0800

    testing function live

--- api/drive/list.js
+++ api/drive/list.js
@@ -1,5 +1,24 @@
-const drive = require(`../../utils/drive/index.js`);
+// const { google } = require(`googleapis`);
 
-module.exports = {
-	drive
+const drive = () => {
+	// const credentials = JSON.parse(process.env.NEXT_GOOGLE_CREDS);
+	// const client = google.auth.getClient({
+	// 	credentials,
+	// 	scopes: [`https://www.googleapis.com/auth/drive.readonly`]
+	// });
+
+	// return google.drive({
+	// 	version: `v3`,
+	// 	auth: client
+	// });
+};
+
+exports.handler = (event, context) => {
+	context.log(event);
+	context.log(context);
+
+	return {
+		statusCode: 200,
+		body: drive
+	};
 };
