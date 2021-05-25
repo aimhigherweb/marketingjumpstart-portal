65f8f6fd271fe627a4dd25b6671013db4b022a4f
Amy Kapernick <amy@aimhigherweb.design>
2021-05-25 14:32:59 +0800

    check for object first

--- components/parts/dashboard/googleAds/index.js
+++ components/parts/dashboard/googleAds/index.js
@@ -19,27 +19,43 @@ const GoogleAds = () => {
 
 	if (isLoading) return <p>Loading...</p>;
 
-	if (error || !data) return <p>Something went wrong</p>;
-
-	console.log(data);
+	if (error || !data || data === {}) return <p>Something went wrong</p>;
 
 	return (
 		<Fragment>
-			<dl className={styles.stats} style={{ '--stats': 4 }}>
-				<dt>Clicks</dt>
-				<dd>{data.clicks}</dd>
-				<dt>Avg CPC</dt>
-				<dd>
-					<span className={styles.type}>$</span>
-					{data.avg_cpc.toFixed(2)}
-				</dd>
-				<dt>Impressions</dt>
-				<dd>{data.impressions}</dd>
-				<dt>Avg CPM</dt>
-				<dd>
-					<span className={styles.type}>$</span>
-					{data.avg_cpm.toFixed(2)}
-				</dd>
+			<dl className={styles.stats} style={{ '--stats': Object.keys(data) .length }}>
+
+				{data.clicks
+				&& <Fragment>
+					<dt>Clicks</dt>
+					<dd>{data.clicks}</dd>
+				</Fragment>
+				}
+				{data.avg_cpc
+				&& <Fragment>
+					<dt>Avg CPC</dt>
+					<dd>
+						<span className={styles.type}>$</span>
+						{data.avg_cpc.toFixed(2)}
+					</dd>
+				</Fragment>
+				}
+
+				{data.impressions
+				&& <Fragment>
+					<dt>Impressions</dt>
+					<dd>{data.impressions}</dd>
+				</Fragment>
+				}
+				{data.avg_cpm
+					&& <Fragment>
+						<dt>Avg CPM</dt>
+						<dd>
+							<span className={styles.type}>$</span>
+							{data.avg_cpm.toFixed(2)}
+						</dd>
+					</Fragment>
+				}
 			</dl>
 		</Fragment>
 	);
